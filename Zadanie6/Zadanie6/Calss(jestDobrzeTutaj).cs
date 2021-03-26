50cad2ae1bcdf0a191b88f7116a4b20e64dd7649
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-03-26 08:38:59 +0100

    Add files via upload

---
+++ Zadanie6/Zadanie6/Calss(jestDobrzeTutaj).cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Zadanie6
+{
+    class Prizes
+    {
+        public char prize;
+        public int height;
+        public int width;
+        public char[] prizeSymbols = { '*', '+', '#' };
+        int sum;
+
+        public Prizes(){
+
+            Random los = new Random();
+            this.prize = prizeSymbols[los.Next(3)];
+            this.height = los.Next(Console.WindowHeight);
+            this.width = los.Next(Console.WindowWidth);
+
+        }
+
+        public Prizes(char prize, int height, int width)
+        {
+
+            this.prize = prize;
+            this.height = height;
+            this.width = width;
+
+        }
+
+        public char Print(Prizes[] prize, int i)
+        {
+            for (int j = 0; j < prize.Length; j++)
+            {
+                Console.SetCursorPosition(width, height);
+
+                if ((prize[i - 1].width == prize[i].width) && (prize[i - 1].height == prize[i].height))
+                    return 'X';
+                else
+                    return prize[i].prize;
+            }
+            return '@';
+
+        }
+
+        static public int Sum(Prizes[] prize) {
+
+        int sum=0;
+
+
+        for (int i = 0; i < prize.Length; i++)
+            {
+                if (prize[i].prize == '*')
+                    sum += 10;
+                if (prize[i].prize == '+')
+                    sum += 5;
+                if (prize[i].prize == '#')
+                    sum += 2;
+             }
+
+            return sum;
+
+        }
+
+    }
+}
---
+++ Zadanie6/Zadanie6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Prizes[] tabOfPrizes = new Prizes[10];
+
+            tabOfPrizes[0] = new Prizes('#', 3, 5);
+            tabOfPrizes[1] = new Prizes('*', 3, 5);
+            tabOfPrizes[2] = new Prizes('*', 7, 12);
+            tabOfPrizes[3] = new Prizes('+', 15, 24);
+            tabOfPrizes[4] = new Prizes('+', 19, 25);
+            tabOfPrizes[5] = new Prizes('#', 12, 12);
+            for (int i = 6; i < tabOfPrizes.Length; i++)
+            {
+                tabOfPrizes[i] = new Prizes();
+            }
+
+            for (int i = 1; i < tabOfPrizes.Length; i++)
+            {
+                Console.WriteLine(tabOfPrizes[i].Print(tabOfPrizes, i));
+            }
+
+            String title = $"{Prizes.Sum(tabOfPrizes)} ZNAKI W KONSOLI";
+            Console.Title = title;
+
+            Console.ReadKey(false);
+
+        }
+    }
+}
